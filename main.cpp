2f7b66c28845e8aef39e1dc16144ef0eb03a825e
mktuwai <kmarlawn@gmail.com>
2026-05-19 20:39:10 +0300

    Updates

--- main.cpp
+++ main.cpp
@@ -53,8 +53,7 @@ int main()
 {
     Student s1;
 
-    s1.setPersonDetails("Mallon Tuwai", 22);
-
+    s1.setPersonDetails("Mallon Tuwai", 25);
     s1.setStudentDetails(
         "ENE211-0038/2021",
         "Electrical and Electronic Engineering",
